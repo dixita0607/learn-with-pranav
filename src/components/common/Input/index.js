5066420d2237ac2a1248bc1635915003ccc819de
Dixita Ganatra <dixita0607@gmail.com>
2020-11-17 21:29:51 +0530

    Modify contact form and implement netlify forms

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -8,14 +8,22 @@ export const Input = styled.input`
   border-radius: 7px;
   margin-bottom: 0.5rem;
   transition: 0.3s;
-
-  ${({ error }) =>
-    error &&
-    `
-		border-color: #ff4136;
-	`}
-
   &::placeholder {
     color: #a7a7a7;
   }
 `;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  box-sizing: border-box;
+  border: 2px solid rgb(0, 116, 222);
+  padding: 0.8rem 1rem;
+  border-radius: 7px;
+  margin-bottom: 0.5rem;
+  transition: 0.3s;
+  min-height: 100px;
+  resize: none;
+  &::placeholder {
+    color: #a7a7a7;
+  }
+`
