483a7660063bdaefc6c5d32aa79d648ad9f4f2b2
Dixita Ganatra <dixita0607@gmail.com>
2020-11-17 21:46:54 +0530

    Modify gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-favicon',
       options: {
-        logo: './static/favicon/favicon-512.png',
+        logo: './static/favicon/favicon.png',
         injectHTML: true,
         icons: {
           android: true,
@@ -56,7 +56,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
-        icon: './static/favicon/favicon-512.png',
+        icon: './static/favicon/favicon.png',
       },
     },
     'gatsby-plugin-offline',
